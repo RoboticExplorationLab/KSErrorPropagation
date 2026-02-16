6296aff513d5f90eddcf6920196652e6e7772b73
João Marafuz Gaspar <jfm.gaspar@gmail.com>
2026-02-16 15:40:35 +0000

    feat(julia): add sanity_check configs

---
+++ julia/config/sanity_check_3.jl
@@ -0,0 +1,8 @@
+include(joinpath(@__DIR__, "default.jl"))
+
+POSITION_UNCERTAINTIES = [1e0]
+NUM_MC_SAMPLES = 100
+
+function compute_velocity_uncertainty(σ_pos, sma, r_vec_0, GM)
+    return 1e-5
+end
